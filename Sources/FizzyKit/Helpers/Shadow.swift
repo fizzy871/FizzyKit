8a1d5461af5ff302e3b3dcf315ea3b1d48d882e1
Alexey Saechnikov <>
2020-09-13 13:44:48 +0300

    fix

--- Sources/FizzyKit/Helpers/Shadow.swift
+++ Sources/FizzyKit/Helpers/Shadow.swift
@@ -7,6 +7,14 @@ public struct Shadow {
     let radius: CGFloat
     let path: CGPath
     
+    public init(color: CGColor, opacity: Float, offset: CGSize, radius: CGFloat, path: CGPath) {
+        self.color = color
+        self.opacity = opacity
+        self.offset = offset
+        self.radius = radius
+        self.path = path
+    }
+    
     public func applyShadow(to view: UIView) {
         view.layer.shadowColor = self.color
         view.layer.shadowOpacity = self.opacity
